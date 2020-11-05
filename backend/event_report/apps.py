69c22601067a049cd9907ee856f0b28524283323
Ariadne <zeuskimone@gmail.com>
2020-11-05 11:53:59 +0900

    backend apps

---
+++ backend/event_report/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EventReportConfig(AppConfig):
+    name = 'event_report'
---
+++ backend/group_report/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GroupReportConfig(AppConfig):
+    name = 'group_report'
