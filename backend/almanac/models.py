b26141be28bc7586e802feefc9018b0fbb3df28c
Ariadne <zeuskimone@gmail.com>
2020-11-07 19:53:16 +0900

    change in model

--- backend/almanac/models.py
+++ backend/almanac/models.py
@@ -2,6 +2,16 @@
 a standard docstring
 '''
 
-# from django.db import models
+from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+
+    '''
+    a class docstring
+    '''
+
+    title = models.CharField(max_length=120)
+    place = models.CharField(max_length=120)
--- backend/almanac/urls.py
+++ backend/almanac/urls.py
@@ -8,4 +8,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.index, name='signup'),
+    path('signin/', views.index, name='signin'),
 ]
