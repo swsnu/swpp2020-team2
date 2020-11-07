8ac51f00ec710fd8394a8e94c5a19e310336cc36
Ariadne <zeuskimone@gmail.com>
2020-11-07 21:27:37 +0900

    change

--- backend/almanac/urls.py
+++ backend/almanac/urls.py
@@ -8,6 +8,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup/', views.index, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('signin/', views.index, name='signin'),
 ]
--- backend/almanac/views.py
+++ backend/almanac/views.py
@@ -3,9 +3,27 @@ a standard docstring
 '''
 
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
+# from django.contrib.auth import authenticate, login, logout
+import json
+from django.conf import settings
 
 # Create your views here.
+
+def signup(request):
+
+    '''
+    a function docstring
+    '''
+
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+        settings.AUTH_USER_MODEL.objects.create_user(username=username, password=password)
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['POST'])
 
 def index(request):
 
