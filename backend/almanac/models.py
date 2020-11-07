1994af62bb2246c37c7e8341eaa13bba84b7b8d0
Ariadne <zeuskimone@gmail.com>
2020-11-07 20:50:34 +0900

    model plus

--- backend/almanac/models.py
+++ backend/almanac/models.py
@@ -3,9 +3,49 @@ a standard docstring
 '''
 
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class University(models.Model):
+
+    '''
+    a class docstring
+    '''
+
+    name = models.CharField(max_length=30)
+
+class Department(models.Model):
+
+    '''
+    a class docstring
+    '''
+
+    name = models.CharField(max_length=30)
+
+class User_Preference(models.Model):
+
+    '''
+    a class docstring
+    '''
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        relate_name='user_set',
+    )
+    university = models.ForeignKey(
+        University,
+        on_delete=models.CASCADE,
+        relate_name='university_set',
+    )
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        relate_name='department_set',
+    )
+    title = models.CharField(max_length=64)
+    place = models.TextField()
 
 class Event(models.Model):
 
@@ -22,7 +62,7 @@ class Category(models.Model):
     a class docstring
     '''
 
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
 
 class Tag(models.Model):
 
@@ -30,4 +70,4 @@ class Tag(models.Model):
     a class docstring
     '''
 
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
