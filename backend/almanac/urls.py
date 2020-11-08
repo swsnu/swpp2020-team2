04e6bcd8b5b5354d860bf142a844ada2523d798c
Ariadne <zeuskimone@gmail.com>
2020-11-08 16:00:54 +0900

    edit views

--- backend/almanac/urls.py
+++ backend/almanac/urls.py
@@ -9,5 +9,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('signup/activate/<uidb64>/<token>', views.activate, name='activate'),
     path('signin/', views.index, name='signin'),
 ]
--- backend/almanac/views.py
+++ backend/almanac/views.py
@@ -6,11 +6,30 @@ a standard docstring
 import json
 from django.http import HttpResponse, HttpResponseNotAllowed
 # from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
 
 # Create your views here.
 
 def signup(request):
+
+    '''
+    a function docstring
+    '''
+
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        first_name = req_data['first_name']
+        last_name = req_data['last_name']
+        password = req_data['password']
+        email = req_data['email']
+        settings.AUTH_USER_MODEL.objects.create_user(is_active=False, username=username,
+        first_name=first_name, last_name=last_name, password=password, email=email)
+        return HttpResponse(status=201)
+    return HttpResponseNotAllowed(['POST'])
+
+def activate(request, uidb64, token):
 
     '''
     a function docstring
