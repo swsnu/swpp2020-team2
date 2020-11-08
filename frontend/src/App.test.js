fbec216670f3e8430efdd6faee394c1633de031b
ray017 <ray017@LAPTOP-V7DL32V1.localdomain>
2020-11-08 20:01:13 +0900

    added App.test

---
+++ frontend/src/App.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+
+import App from './App';
+import { getMockStore } from './test-utils/mocks';
+import { history } from './store/store';
+
+jest.mock('./containers/login/Login', () => {
+  return jest.fn(props => {
+    return (<div className="spyLogin"/>);
+  });
+});
+
+describe('App',()=>{
+  const mockedstate={};
+  const mockedStore=getMockStore(mockedstate);
+  function makeComponent(store){
+    return <Provider store={store}><App history={history}/></Provider>
+  }
+
+  it('should render without error', () => {
+    const component=mount(makeComponent(mockedStore));
+    expect(component.find('.App').length).toBe(1);
+  });
+
+  it(`should route to 'Not Found' page by inappropriate url`,()=>{
+    history.push('./ray017');
+    const component=mount(makeComponent(mockedStore));
+    expect(component.find('h1').text()).toBe('Not Found');
+  });
+})
+
+
