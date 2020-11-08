f166034e032eb657f5ea7e402c4c3a9f4e0165df
Ariadne <zeuskimone@gmail.com>
2020-11-08 11:54:46 +0900

    models.py change

--- backend/almanac/models.py
+++ backend/almanac/models.py
@@ -3,7 +3,6 @@ a standard docstring
 '''
 
 from django.db import models
-# from django.contrib.auth.models import User
 from django.conf import settings
 
 # Create your models here.
@@ -15,6 +14,7 @@ class University(models.Model):
     '''
 
     name = models.CharField(max_length=30)
+    domain = models.CharField(max_length=30)
 
 class Department(models.Model):
 
