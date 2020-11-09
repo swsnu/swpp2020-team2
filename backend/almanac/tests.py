e1bb1a36a493778a6210c2a543fe4d4915cffa80
Ariadne <zeuskimone@gmail.com>
2020-11-09 23:18:41 +0900

    modified test code

--- backend/almanac/tests.py
+++ backend/almanac/tests.py
@@ -3,15 +3,17 @@ a standard docstring
 '''
 
 import json
-from django.test import TestCase, Client
+from django.test import TransactionTestCase, TestCase, Client
 from django.contrib.auth import get_user_model
 from django.core import mail
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 
 # Create your tests here.
 
 User = get_user_model()
 
-class AlmanacTestCase(TestCase):
+class AlmanacCsrfTestCase(TestCase):
 
     '''
     a class docstring
@@ -53,9 +55,32 @@ class AlmanacTestCase(TestCase):
             content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Almanac Email Verification')
-        self.assertEqual(len(mail.outbox[0].to), 1)
-        self.assertEqual(mail.outbox[0].to[0], 'taekop@snu.ac.kr')
+        mail_sent = mail.outbox[0]
+        self.assertEqual(mail_sent.subject, 'Almanac Email Verification')
+        self.assertEqual(len(mail_sent.to), 1)
+        self.assertEqual(mail_sent.to[0], 'taekop@snu.ac.kr')
+        user = User.objects.get(username='taekop')
+        body_pt = ('Hello, taekop. Welcome to the Almanac Service. You can activate your account '
+        'via the link \nhttps://localhost/signup/{}/').format(
+            urlsafe_base64_encode(force_bytes(user.id)))
+        self.assertIn(body_pt, mail_sent.body)
+
+class AlmanacSignupTestCase(TransactionTestCase):
+
+    '''
+    a class docstring
+    '''
+
+    def setUp(self):
+
+        '''
+        a function docstring
+        '''
+
+        User.objects.create(
+            username='ray017', first_name='Raegeon',
+            last_name='Lee', password='password', email='cbda117@snu.ac.kr', is_active=False)
+
 
     def test_signup(self):
 
@@ -73,6 +98,18 @@ class AlmanacTestCase(TestCase):
 
         response = client.post('/signup/', json.dumps(
             {'username': 'taekop', 'first_name': 'Seungtaek',
+            'last_name': 'Oh', 'password': 'password2', 'email': 'taekop@snu.ac.kr'}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        response = client.post('/signup/', json.dumps(
+            {'username': 'taekop', 'first_name': 'Seungtaek',
+            'last_name': 'Oh', 'password': 'password2', 'email': 'taekop@snu.ac.kr'}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+        response = client.post('/signup/', json.dumps(
+            {'username': 'taekop2', 'first_name': 'Seungtaek',
             'last_name': 'Oh', 'password': 'password2', 'email': 'taekop@snu.ac.kr'}),
             content_type='application/json')
         self.assertEqual(response.status_code, 201)
