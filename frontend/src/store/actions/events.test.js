c9394d360a32597e6f7ec997bb056d5c87771cda
ray017 <ray017@LAPTOP-V7DL32V1.localdomain>
2020-11-18 20:26:15 +0900

    fixed eslint errors

--- frontend/src/store/actions/events.test.js
+++ frontend/src/store/actions/events.test.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 import store from '../store';
 import * as actionCreators from './events';
 
-describe('events',()=>{
+describe('events', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('getEvent should operate correctly',(done)=>{
+  it('getEvent should operate correctly', (done) => {
     const spyOnGet = jest.spyOn(axios, 'get')
       .mockImplementation((url) => new Promise((resolve, reject) => {
         const result = {
           status: 200,
-          data: {id:1, title:"test_title"},
+          data: { id: 1, title: 'test_title' },
         };
         resolve(result);
       }));
-    
-    store.dispatch(actionCreators.getEvent(1)).then(()=>{
+
+    store.dispatch(actionCreators.getEvent(1)).then(() => {
       expect(spyOnGet).toHaveBeenCalledTimes(1);
       done();
     });
