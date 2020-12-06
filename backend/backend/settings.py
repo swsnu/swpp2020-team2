626b7c3c3da844539ba6ce1df54cc7ebf803e09a
DongminShin <sdm369@naver.com>
2020-12-07 00:00:58 +0900

    add Allowed host_sdm1230

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tcs0p9_%j%ikjzsf@_z--gi2*@_veg!3vvk=49y7%k-^r)0$tx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-34-229-74-66.compute-1.amazonaws.com']
 
 
 # Application definition
