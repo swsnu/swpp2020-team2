42606c773f287004e742185639a70a35d758e6a7
DongminShin <sdm369@naver.com>
2020-12-08 21:57:34 +0900

    edit settings.py for django css

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+from sys  import os
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Email backend
