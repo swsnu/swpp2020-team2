919db9e67077ab7c00fef6963d6ca378407dfdef
ray017 <ray017@LAPTOP-V7DL32V1.localdomain>
2020-12-16 16:01:47 +0900

    finished member manage(please)

--- frontend/src/components/groupBox/GroupBox.js
+++ frontend/src/components/groupBox/GroupBox.js
@@ -9,11 +9,6 @@ import { RiAlarmWarningFill } from 'react-icons/ri';
 import {BsBellFill} from 'react-icons/bs'
 import {AiFillLike} from 'react-icons/ai'
 
-
-
-// should implement groupbox, which will show group name, description, (like, join request, notice and report) buttons.
-// props will have things like group name, description, handlers for buttons(and image).
-
 const GroupBox = (props) => (
   <div className="GroupBox">
     <img src={props.image} alt="group logo" onClick={() => props.detail()} />
