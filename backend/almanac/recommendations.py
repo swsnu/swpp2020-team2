ee3ac1c8a5c1e1e4a6b2405fa08dfd8bad0108ca
Ariadne <zeuskimone@gmail.com>
2020-12-16 10:09:44 +0900

    changed logic

--- backend/almanac/recommendations.py
+++ backend/almanac/recommendations.py
@@ -15,8 +15,13 @@ clf_ovr = Pipeline([('vect', CountVectorizer(stop_words='english',max_features=7
                 ('tfidf', TfidfTransformer(use_idf=True)),
                 ('clf', OneVsRestClassifier(SVC(kernel='linear')))])
 
-int_to_tag = []
-tag_to_int = {}
+INT_TO_TAG = []
+TAG_TO_INT = {}
+
+def add_tag(tag_id):
+    
+    INT_TO_TAG.append(tag_id)
+    TAG_TO_INT[tag_id] = len(INT_TO_TAG)
 
 def refresh_tag():
 
@@ -24,16 +29,15 @@ def refresh_tag():
     a function docstring
     '''
 
-    global int_to_tag, tag_to_int
-    int_to_tag = []
-    tag_to_int = {}
+    global INT_TO_TAG, TAG_TO_INT
+    INT_TO_TAG = []
+    TAG_TO_INT = {}
     idx = 1
 
     for t_id in Tag.objects.values_list('id', flat=True):
-        int_to_tag.append(t_id)
-        tag_to_int[t_id] = idx
+        INT_TO_TAG.append(t_id)
+        TAG_TO_INT[t_id] = idx
         idx += 1
-
 
 def recommend_tag(content, num=3):
 
@@ -48,7 +52,7 @@ def recommend_tag(content, num=3):
         } for e in Event.objects.prefetch_related('tag').order_by('-date')]
 
     train_data = [event['content'] for event in events[:100]]
-    train_target = [[tag_to_int[t] for t in event['tag']] for event in events]
+    train_target = [[TAG_TO_INT[t] for t in event['tag']] for event in events]
     if train_data:
         clf_ovr.fit(train_data, MultiLabelBinarizer().fit_transform(train_target))
 
@@ -56,5 +60,5 @@ def recommend_tag(content, num=3):
     pre_result = list(zip(
         *heapq.nlargest(num, enumerate(predicted[0]), key=operator.itemgetter(1))
     ))[0]
-    result = list(map(lambda x: int_to_tag[x], pre_result))
+    result = list(map(lambda x: INT_TO_TAG[x], pre_result))
     return result
